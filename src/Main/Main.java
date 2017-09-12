f7354b226588441d6588e607df0041ec10242082
Xavier Negre <blackbomba32@hotmail.fr>
2017-09-12 11:00:41 +0200

    Main Commit
    Main 1st Commit

---
+++ src/Main/Main.java
@@ -0,0 +1,10 @@
+package Main;
+
+/**
+ * Created by WeedBreed on 12/09/2017.
+ */
+public class Main {
+    public static void main(String args) {
+
+    }
+}
---
+++ src/api/Utile.java
@@ -0,0 +1,8 @@
+package api;
+
+/**
+ * Created by WeedBreed on 12/09/2017.
+ */
+public class Utile {
+    public final String MAIN_PATH = "albiononlinedb.com/api/items/";
+}
