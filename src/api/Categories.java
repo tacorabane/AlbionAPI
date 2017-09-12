499b1a221cfbbcbca6e3afd51d61cc5b6fc5854e
Xavier Negre <blackbomba32@hotmail.fr>
2017-09-12 12:49:13 +0200

    Categories added
    Categories done
    Items path for the api in progress

---
+++ src/api/Categories.java
@@ -0,0 +1,24 @@
+package api;
+
+/**
+ * Created by WeedBreed on 12/09/2017.
+ */
+public class Categories {
+    public final String ARMOR_CAT = "Armor";
+    public final String GATHERING_GEAR_CAT = "Gathrering Gear";
+    public final String CONSUMABLE_CAT = "Consumable";
+    public final String OFF_HAND_CAT = "Off-Hand";
+    public final String MOUNT_CAT = "Mount";
+    public final String MAGIC_CAT = "Magic";
+    public final String MELEE_CAT = "Melee";
+    public final String RANGED_CAT = "Ranged";
+    public final String TOOL_CAT = "Tool";
+    public final String ACCESSORIES_CAT = "Accessories";
+    public final String ARTIFACT_CAT = "Artifact";
+    public final String FARMABLE_CAT = "Farmable";
+    public final String FURNITURE_CAT = "Furniture";
+    public final String MATERIALS_CAT = "Materials";
+    public final String OTHER_CAT = "Other";
+    public final String RESOURCE_CAT = "Resource";
+    public final String TROPHIES_CAT = "Trophies";
+}
