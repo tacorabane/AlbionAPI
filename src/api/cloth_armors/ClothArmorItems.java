f3ebf20394dbde45161256043f6ae1892dc86f6b
Xavier Negre <blackbomba32@hotmail.fr>
2017-09-14 17:12:00 +0200

    Medium Modification
    Finished leather items
    Started cloths Items
       -> Armors
       -> Cowls

---
+++ src/api/cloth_armors/ClothArmorItems.java
@@ -0,0 +1,161 @@
+package api.cloth_armors;
+
+/**
+ * Created by WeedBreed on 14/09/2017.
+ */
+public class ClothArmorItems {
+    /** Cloth parts */
+    public final String NOVICES_SCHOLAR_ROBE = "T2_ARMOR_CLOTH_SET1";
+    public final String JOURNEYMANS_SCHOLAR_ROBE = "T3_ARMOR_CLOTH_SET1";
+
+    public final String ADEPTS_FIEND_ROBE1 = "T4_ARMOR_CLOTH_HELL";
+    public final String ADEPTS_FIEND_ROBE2 = "T4_ARMOR_CLOTH_HELL@1";
+    public final String ADEPTS_FIEND_ROBE3 = "T4_ARMOR_CLOTH_HELL@2";
+    public final String ADEPTS_FIEND_ROBE4 = "T4_ARMOR_CLOTH_HELL@3";
+
+    public final String EXPERTS_FIEND_ROBE1 = "T5_ARMOR_CLOTH_HELL";
+    public final String EXPERTS_FIEND_ROBE2 = "T5_ARMOR_CLOTH_HELL@1";
+    public final String EXPERTS_FIEND_ROBE3 = "T5_ARMOR_CLOTH_HELL@2";
+    public final String EXPERTS_FIEND_ROBE4 = "T5_ARMOR_CLOTH_HELL@3";
+
+    public final String MASTERS_FIEND_ROBE1 = "T6_ARMOR_CLOTH_HELL";
+    public final String MASTERS_FIEND_ROBE2 = "T6_ARMOR_CLOTH_HELL@1";
+    public final String MASTERS_FIEND_ROBE3 = "T6_ARMOR_CLOTH_HELL@2";
+    public final String MASTERS_FIEND_ROBE4 = "T6_ARMOR_CLOTH_HELL@3";
+
+    public final String GRANDMASTERS_FIEND_ROBE1 = "T7_ARMOR_CLOTH_HELL";
+    public final String GRANDMASTERS_FIEND_ROBE2 = "T7_ARMOR_CLOTH_HELL@1";
+    public final String GRANDMASTERS_FIEND_ROBE3 = "T7_ARMOR_CLOTH_HELL@2";
+    public final String GRANDMASTERS_FIEND_ROBE4 = "T7_ARMOR_CLOTH_HELL@3";
+
+    public final String ELDERS_FIEND_ROBE1 = "T8_ARMOR_CLOTH_HELL";
+    public final String ELDERS_FIEND_ROBE2 = "T8_ARMOR_CLOTH_HELL@1";
+    public final String ELDERS_FIEND_ROBE3 = "T8_ARMOR_CLOTH_HELL@2";
+    public final String ELDERS_FIEND_ROBE4 = "T8_ARMOR_CLOTH_HELL@3";
+
+    public final String ADEPTS_SCHOLAR_ROBE1 = "T4_ARMOR_CLOTH_SET1";
+    public final String ADEPTS_SCHOLAR_ROBE2 = "T4_ARMOR_CLOTH_SET1@1";
+    public final String ADEPTS_SCHOLAR_ROBE3 = "T4_ARMOR_CLOTH_SET1@2";
+    public final String ADEPTS_SCHOLAT_ROBE4 = "T4_ARMOR_CLOTH_SET1@3";
+
+    public final String EXPERTS_SCHOLAR_ROBE1 = "T5_ARMOR_CLOTH_SET1";
+    public final String EXPERTS_SCHOLAR_ROBE2 = "T5_ARMOR_CLOTH_SET1@1";
+    public final String EXPERTS_SCHOLAR_ROBE3 = "T5_ARMOR_CLOTH_SET1@2";
+    public final String EXPERTS_SCHOLAR_ROBE4 = "T5_ARMOR_CLOTH_SET1@3";
+
+    public final String MASTERS_SCHOLAR_ROBE1 = "T6_ARMOR_CLOTH_SET1";
+    public final String MASTERS_SCHOLAR_ROBE2 = "T6_ARMOR_CLOTH_SET1@1";
+    public final String MASTERS_SCHOLAR_ROBE3 = "T6_ARMOR_CLOTH_SET1@2";
+    public final String MASTERS_SCHOLAR_ROBE4 = "T6_ARMOR_CLOTH_SET1@3";
+
+    public final String GRANDMASTERS_SCHOLAR_ROBE1 = "T7_ARMOR_CLOTH_SET1";
+    public final String GRANDMASTERS_SCHOLAR_ROBE2 = "T7_ARMOR_CLOTH_SET1@1";
+    public final String GRANDMASTERS_SCHOLAR_ROBE3 = "T7_ARMOR_CLOTH_SET1@2";
+    public final String GRANDMASTERS_SCHOLAR_ROBE4 = "T7_ARMOR_CLOTH_SET1@3";
+
+    public final String ELDERS_SCHOLAR_ROBE1 = "T8_ARMOR_CLOTH_SET1";
+    public final String ELDERS_SCHOLAR_ROBE2 = "T8_ARMOR_CLOTH_SET1@1";
+    public final String ELDERS_SCHOLAR_ROBE3 = "T8_ARMOR_CLOTH_SET1@2";
+    public final String ELDERS_SCHOLAR_ROBE4 = "T8_ARMOR_CLOTH_SET1@3";
+
+    public final String ADEPTS_CLERIC_ROBE1 = "T4_ARMOR_CLOTH_SET2";
+    public final String ADPETS_CLERIC_ROBE2 = "T4_ARMOR_CLOTH_SET2@1";
+    public final String ADEPTS_CLERIC_ROBE3 = "T4_ARMOR_CLOTH_SET2@2";
+    public final String ADPETS_CLERIC_ROBE4 = "T4_ARMOR_CLOTH_SET2@3";
+
+    public final String EXPERTS_CLERIC_ROBE1 = "T5_ARMOR_CLOTH_SET2";
+    public final String EXPERTS_CLERIC_ROBE2 = "T5_ARMOR_CLOTH_SET2@1";
+    public final String EXPERTS_CLERIC_ROBE3 = "T5_ARMOR_CLOTH_SET2@2";
+    public final String EXPERTS_CLERIC_ROBE4 = "T5_ARMOR_CLOTH_SET2@3";
+
+    public final String MASTERS_CLERIC_ROBE1 = "T6_ARMOR_CLOTH_SET2";
+    public final String MASTERS_CLERIC_ROBE2 = "T6_ARMOR_CLOTH_SET2@1";
+    public final String MASTERS_CLERIC_ROBE3 = "T6_ARMOR_CLOTH_SET2@2";
+    public final String MASTERS_CLERIC_ROBE4 = "T6_ARMOR_CLOTH_SET2@3";
+
+    public final String GRANDMASTERS_CLERIC_ROBE1 = "T7_ARMOR_CLOTH_SET2";
+    public final String GRANDMASTERS_CLERIC_ROBE2 = "T7_ARMOR_CLOTH_SET2@1";
+    public final String GRANDMASTERS_CLERIC_ROBE3 = "T7_ARMOR_CLOTH_SET2@2";
+    public final String GRANDMASTERS_CLERIC_ROBE4 = "T7_ARMOR_CLOTH_SET2@3";
+
+    public final String ELDERS_CLERIC_ROBE1 = "T8_ARMOR_CLOTH_SET2";
+    public final String ELDERS_CLERIC_ROBE2 = "T8_ARMOR_CLOTH_SET2@1";
+    public final String ELDERS_CLERIC_ROBE3 = "T8_ARMOR_CLOTH_SET2@2";
+    public final String ELDERS_CLERIC_ROBE4 = "T8_ARMOR_CLOTH_SET2@3";
+
+    public final String ADEPTS_MAGE_ROBE1 = "T4_ARMOR_CLOTH_SET3";
+    public final String ADPETS_MAGE_ROBE2 = "T4_ARMOR_CLOTH_SET3@1";
+    public final String ADEPTS_MAGE_ROBE3 = "T4_ARMOR_CLOTH_SET3@2";
+    public final String ADEPTS_MAGE_ROBE4 = "T4_ARMOR_CLOTH_SET3@3";
+
+    public final String EXPERTS_MAGE_ROBE1 = "T5_ARMOR_CLOTH_SET3";
+    public final String EXPERTS_MAGE_ROBE2 = "T5_ARMOR_CLOTH_SET3@1";
+    public final String EXPERTS_MAGE_ROBE3 = "T5_ARMOR_CLOTH_SET3@2";
+    public final String EXPERTS_MAGE_ROBE4 = "T5_ARMOR_CLOTH_SET3@3";
+
+    public final String MASTERS_MAGE_ROBE1 = "T6_ARMOR_CLOTH_SET3";
+    public final String MASTERS_MAGE_ROBE2 = "T6_ARMOR_CLOTH_SET3@1";
+    public final String MASTERS_MAGE_ROBE3 = "T6_ARMOR_CLOTH_SET3@2";
+    public final String MASTERS_MAGE_ROBE4 = "T6_ARMOR_CLOTH_SET3@3";
+
+    public final String GRANDMASTERS_MAGE_ROBE1 = "T7_ARMOR_CLOTH_SET3";
+    public final String GRANDMASTERS_MAGE_ROBE2 = "T7_ARMOR_CLOTH_SET3@1";
+    public final String GRANDMASTERS_MAGE_ROBE3 = "T7_ARMOR_CLOTH_SET3@2";
+    public final String GRANDMASTERS_MAGE_ROBE4 = "T7_ARMOR_CLOTH_SET3@3";
+
+    public final String ELDERS_MAGE_ROBE1 = "T8_ARMOR_CLOTH_SET3";
+    public final String ELDERS_MAGE_ROBE2 = "T8_ARMOR_CLOTH_SET3@1";
+    public final String ELDERS_MAGE_ROBE3 = "T8_ARMOR_CLOTH_SET3@2";
+    public final String ELDERS_MAGE_ROBE4 = "T8_ARMOR_CLOTH_SET3@3";
+
+    public final String ADEPTS_ROYAL_ROBE1 = "T4_ARMOR_CLOTH_ROYAL";
+    public final String ADPETS_ROYAL_ROBE2 = "T4_ARMOR_CLOTH_ROYAL@1";
+    public final String ADEPTS_ROYAL_ROBE3 = "T4_ARMOR_CLOTH_ROYAL@2";
+    public final String ADEPTS_ROYAL_ROBE4 = "T4_ARMOR_CLOTH_ROYAL@3";
+
+    public final String EXPERTS_ROYAL_ROBE1 = "T5_ARMOR_CLOTH_ROYAL";
+    public final String EXPERTS_ROYAL_ROBE2 = "T5_ARMOR_CLOTH_ROYAL@1";
+    public final String EXPERTS_ROYAL_ROBE3 = "T5_ARMOR_CLOTH_ROYAL@2";
+    public final String EXPERTS_ROYAL_ROBE4 = "T5_ARMOR_CLOTH_ROYAL@3";
+
+    public final String MASTERS_ROYAL_ROBE1 = "T6_ARMOR_CLOTH_ROYAL";
+    public final String MASTERS_ROYAL_ROBE2 = "T6_ARMOR_CLOTH_ROYAL@1";
+    public final String MASTERS_ROYAL_ROBE3 = "T6_ARMOR_CLOTH_ROYAL@2";
+    public final String MASTERS_ROYAL_ROBE4 = "T6_ARMOR_CLOTH_ROYAL@3";
+
+    public final String GRANDMASTERS_ROYAL_ROBE1 = "T7_ARMOR_CLOTH_ROYAL";
+    public final String GRANDMASTERS_ROYAL_ROBE2 = "T7_ARMOR_CLOTH_ROYAL@1";
+    public final String GRANDMASTERS_ROYAL_ROBE3 = "T7_ARMOR_CLOTH_ROYAL@2";
+    public final String GRANDMASTERS_ROYAL_ROBE4 = "T7_ARMOR_CLOTH_ROYAL@3";
+
+    public final String ELDERS_ROYAL_ROBE1 = "T8_ARMOR_CLOTH_ROYAL";
+    public final String ELDERS_ROYAL_ROBE2 = "T8_ARMOR_CLOTH_ROYAL@1";
+    public final String ELDERS_ROYAL_ROBE3 = "T8_ARMOR_CLOTH_ROYAL@2";
+    public final String ELDERS_ROYAL_ROBE4 = "T8_ARMOR_CLOTH_ROYAL@3";
+
+
+    public final String ADEPTS_DRUID_ROBE1 = "T4_ARMOR_CLOTH_KEEPER";
+    public final String ADEPTS_DRUID_ROBE2 = "T4_ARMOR_CLOTH_KEEPER@1";
+    public final String ADEPTS_DRUID_ROBE3 = "T4_ARMOR_CLOTH_KEEPER@2";
+    public final String ADEPTS_DRUID_ROBE4 = "T4_ARMOR_CLOTH_KEEPER@3";
+
+    public final String EXPERTS_DRUID_ROBE1 = "T5_ARMOR_CLOTH_KEEPER";
+    public final String EXPERTS_DRUID_ROBE2 = "T5_ARMOR_CLOTH_KEEPER@1";
+    public final String EXPERTS_DRUID_ROBE3 = "T5_ARMOR_CLOTH_KEEPER@2";
+    public final String EXPERTS_DRUID_ROBE4 = "T5_ARMOR_CLOTH_KEEPER@3";
+
+    public final String MASTERS_DRUID_ROBE1 = "T6_ARMOR_CLOTH_KEEPER";
+    public final String MASTERS_DRUID_ROBE2 = "T6_ARMOR_CLOTH_KEEPER@1";
+    public final String MASTERS_DRUID_ROBE3 = "T6_ARMOR_CLOTH_KEEPER@2";
+    public final String MASTERS_DRUID_ROBE4 = "T6_ARMOR_CLOTH_KEEPER@3";
+
+    public final String GRANDMASTERS_DRUID_ROBE1 = "T7_ARMOR_CLOTH_KEEPER";
+    public final String GRANDMASTERS_DRUID_ROBE2 = "T7_ARMOR_CLOTH_KEEPER@1";
+    public final String GRANDMASTERS_DRUID_ROBE3 = "T7_ARMOR_CLOTH_KEEPER@2";
+    public final String GRANDMASTERS_DRUID_ROBE4 = "T7_ARMOR_CLOTH_KEEPER@3";
+
+    public final String ELDERS_DRUID_ROBE1 = "T8_ARMOR_CLOTH_KEEPER";
+    public final String ELDERS_DRUID_ROBE2 = "T8_ARMOR_CLOTH_KEEPER@1";
+    public final String ELDERS_DRUID_ROBE3 = "T8_ARMOR_CLOTH_KEEPER@2";
+    public final String ELDERS_DRUID_ROBE4 = "T8_ARMOR_CLOTH_KEEPER@3";
+}
