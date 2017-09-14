5d16c951b867fab9c6fa4efeda71033eecf7f897
Xavier Negre <blackbomba32@hotmail.fr>
2017-09-14 17:17:21 +0200

    Items class supressed

--- src/api/Items.java
+++
@@ -1,7 +0,0 @@
-package api;
-
-/**
- * Created by WeedBreed on 12/09/2017.
- */
-public class Items {
-}
--- src/api/Utile.java
+++ src/api/Utile.java
@@ -4,5 +4,5 @@ package api;
  * Created by WeedBreed on 12/09/2017.
  */
 public class Utile {
-    public final String MAIN_PATH = "albiononlinedb.com/api/items/";
+    public final String MAIN_PATH = "albiononlinedb.com/api/items/id";
 }
